f35ae79b012938fb13687f42b9f3d0fd44891801
jawehner <jawehner@gmail.com>
2019-01-07 16:02:57 -0500

    Monday 1/7 commit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { RegisterUser } from '../models/RegisterUser';
 import { HttpClient } from '@angular/common/http';
 
-const Api_Url = 'https://localhost:44311/'
+const Api_Url = 'https://localhost:44311/api'
 
 @Injectable({
   providedIn: 'root' 
@@ -15,13 +15,13 @@ export class AuthService {
     return this._http.post(`${Api_Url}/api/Account/Register`, regUserData);
   }
 
-  login(loginInfo) {
-    const str = 
-    `grant_type=password&username=
-    ${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`;
+  // login(loginInfo) {
+  //   const str = 
+  //   `grant_type=password&username=
+  //   ${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`;
     
-    return this._http.post(`${Api_Url}/Token`, str).subscribe( (token: Token) => {
-      localStorage.setItem('id_token', token.access_token);
-    });
-  }
+  //   return this._http.post(`${Api_Url}/Token`, str).subscribe( (token: Token) => {
+  //     localStorage.setItem('id_token', token.access_token);
+  //   });
+  // }
 }
