afc9fb8c319274b311d7b0010029ee8db3fc33d5
jawehner <jawehner@gmail.com>
2019-01-10 15:01:25 -0500

    random styling

--- src/app/components/wishlist/wishlist-create/wishlist-create.component.ts
+++ src/app/components/wishlist/wishlist-create/wishlist-create.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-wishlist-create',
-  templateUrl: './wishlist-create.component.html',
-  styleUrls: ['./wishlist-create.component.css']
-})
-export class WishlistCreateComponent implements OnInit {
+// @Component({
+//   selector: 'app-wishlist-create',
+//   templateUrl: './wishlist-create.component.html',
+//   styleUrls: ['./wishlist-create.component.css']
+// })
+// export class WishlistCreateComponent implements OnInit {
 
-  constructor() { }
+//   constructor() { }
 
-  ngOnInit() {
-  }
-
-}
+//   ngOnInit() {
+//   }
+// }
