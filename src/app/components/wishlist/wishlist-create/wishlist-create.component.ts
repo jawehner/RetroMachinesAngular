1e8332f678fcbfbb0a3c60a3382e44760b10f179
jawehner <jawehner@gmail.com>
2019-01-11 09:09:27 -0500

    wishlist-create, api.service

--- src/app/components/wishlist/wishlist-create/wishlist-create.component.ts
+++ src/app/components/wishlist/wishlist-create/wishlist-create.component.ts
@@ -1,14 +1,26 @@
-// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WishlistsService } from '../../../services/wishlists.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
-// @Component({
-//   selector: 'app-wishlist-create',
-//   templateUrl: './wishlist-create.component.html',
-//   styleUrls: ['./wishlist-create.component.css']
-// })
-// export class WishlistCreateComponent implements OnInit {
+@Component({
+  selector: 'app-wishlist-create',
+  templateUrl: './wishlist-create.component.html',
+  styleUrls: ['./wishlist-create.component.css']
+})
+export class WishlistCreateComponent implements OnInit {
+    
+    wishlistForm: FormGroup;
 
-//   constructor() { }
+  constructor(private _wishlistService: WishlistsService, private _router: Router) { }
 
-//   ngOnInit() {
-//   }
-// }
+  ngOnInit() {
+  }
+
+  onSubmit() {
+      this._wishlistService.createWishlist(this.wishlistForm.value).subscribe(data => {
+          this._router.navigate(['/wishlist']);
+      })
+  }
+  
+}
