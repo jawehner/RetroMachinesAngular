37252576f0bc3c01bf5dab1d90797f79e407b9ca
caldwellusmc <deshanecaldwell@yahoo.com>
2019-01-15 12:25:38 -0500

    delete working

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
-import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
