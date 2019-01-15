33f74f23745d955c8212234e8cb78710132d3ebd
caldwellusmc <deshanecaldwell@yahoo.com>
2019-01-15 16:46:42 -0500

    hid buttons on detail view

--- src/app/components/product/product-detail/product-detail.component.ts
+++ src/app/components/product/product-detail/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import { ProductsService } from 'src/app/services/products.service';
 import { Product } from 'src/app/models/Product';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,14 +12,20 @@ import { Product } from 'src/app/models/Product';
 export class ProductDetailComponent implements OnInit {
 
   product: Product;
+  token: any;
+  decodedToken: any;
+  userId: any;
 
-  constructor(private _activatedRoute: ActivatedRoute, private _productService: ProductsService) { }
+  constructor(private _activatedRoute: ActivatedRoute, private _productService: ProductsService, private _jwtHelper: JwtHelperService) { }
 
   ngOnInit() {
+    this.token = localStorage.getItem('id_token');
+    this.decodedToken = this._jwtHelper.decodeToken(this.token);
+    this.userId = this.decodedToken.nameid;
     this._activatedRoute.paramMap.subscribe(routeData => {
-      console.log(routeData.get('id'));
       this._productService.getProduct(routeData.get('id')).subscribe((singleProduct: Product) =>{
         this.product = singleProduct;
+        console.log(this.product)
         // console.log(routeData);
       })
     })
