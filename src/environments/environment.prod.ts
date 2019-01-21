af123d760691a1e5db34b41fae64bc10f993ea27
caldwellusmc <deshanecaldwell@yahoo.com>
2019-01-21 11:17:05 -0500

    deploying

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+export let Api_Url= '';
+
+switch (window.location.hostname) {
+  case 'retro-machines.herokuapp.com':
+      Api_Url= 'https://retro-machines.herokuapp.com'
+      break;
+  default:
+      Api_Url= 'http://localhost:44311/api';
+}
+
 export const environment = {
   production: true
 };
