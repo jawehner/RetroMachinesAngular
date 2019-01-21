df0068a4d513026abcc5c0ddb45217bee4e72227
caldwellusmc <deshanecaldwell@yahoo.com>
2019-01-21 15:32:45 -0500

    changed env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export let Api_Url= '';
 
 switch (window.location.hostname) {
   case 'retro-machines.herokuapp.com':
-      Api_Url= 'https://retro-machines.herokuapp.com'
+      Api_Url= 'https://retromachinesapi.azurewebsites.net'
       break;
   default:
       Api_Url= 'http://localhost:44311/api';
