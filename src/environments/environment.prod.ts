0ee7d2c0b4ebc929a262810833c16e79e0436fc8
caldwellusmc <deshanecaldwell@yahoo.com>
2019-01-22 10:00:48 -0500

    added api to url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export let Api_Url= '';
 
 switch (window.location.hostname) {
   case 'retro-machines.herokuapp.com':
-      Api_Url= 'https://retromachinesapi.azurewebsites.net'
+      Api_Url= 'https://retromachinesapi.azurewebsites.net/api'
       break;
   default:
-      Api_Url='https://retromachinesapi.azurewebsites.net' ;
+      Api_Url='https://retromachinesapi.azurewebsites.net/api' ;
 }
 
 export const environment = {
